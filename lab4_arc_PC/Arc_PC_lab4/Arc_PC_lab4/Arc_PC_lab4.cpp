79f3ed3b95addb50bd9f951d175ddecbd49e8a45
MaksimLor <lormanmax@gmail.com>
2024-05-31 20:10:42 +0300

    Add files via upload

---
+++ lab4_arc_PC/Arc_PC_lab4/Arc_PC_lab4/Arc_PC_lab4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+
+int main() {
+    std::string source = "C:\\Users\\User\\lab3\\ПТ-23\\Lorman Maksym\\lab4_arc_PC\\Not hidden folder";
+    std::string destination = "C:\\Users\\User\\lab3\\ПТ-23\\Lorman Maksym\\batch\\Not hidden folder";
+
+    std::string newDirPath = "C:\\Users\\User\\lab3\\ПТ-23\\Lorman Maksym\\lab4_arhPC";
+
+    if (!fs::exists(newDirPath))
+        fs::create_directory(newDirPath);
+
+    try {
+        fs::copy(source, destination, fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+        std::cout << "Files copied successfully. Catalog path: C:\\Users\\User\\lab3\\ПТ-23\\Lorman Maksym\\batch\\Not hidden folder  " << std::endl;
+    }
+    catch (const fs::filesystem_error& e) {
+        std::cerr << "Error copying files: Check the name of ways to files!!!" << std::endl;
+    }
+
+    return 0;
+}
